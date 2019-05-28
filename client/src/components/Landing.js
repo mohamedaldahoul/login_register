d3d0d1d446c0b165629b3f07a7edb1e5322b6d69
mohamedaldahoul <ma811forever@gmail.com>
2019-05-28 19:51:16 +0200

    fix the client and the react app

---
+++ client/src/components/Landing.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+export default class Landing extends Component {
+  render(){
+    return(
+      <div className="container">
+        <div className="jumbotron mt-5">
+          <div className="col-sm8 mx-auto">
+            <h1 className="text-center">Welcome</h1>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
